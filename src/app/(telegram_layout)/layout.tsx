98a4f921eecda319715aef0fd34c6e544532eacb
Iurii Venger <venger.yuriy@icloud.com>
2024-09-28 00:20:45 +0200

    update script

--- src/app/(telegram_layout)/layout.tsx
+++ src/app/(telegram_layout)/layout.tsx
@@ -3,7 +3,7 @@
 import 'react-toastify/dist/ReactToastify.css';
 import { SDKProvider } from '@telegram-apps/sdk-react';
 import Script from 'next/script';
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useState } from 'react';
 
 import TelegramInit from './_components/TelegramInit';
 
@@ -12,15 +12,21 @@ import { BrandLoader } from '@/components/ui/Loader';
 
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
-const MainLayout: FC<RootLayoutProps> = ({ children }) => (
-  <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
-    <SDKProvider>
-      <Script onLoad={() => console.log('script loaded')} src="https://telegram.org/js/telegram-web-app.js" />
-      <TelegramInit />
-      <Suspense fallback={<BrandLoader />}>{children}</Suspense>
-      <LayoutModalContainer />
-    </SDKProvider>
-  </main>
-);
+const MainLayout: FC<RootLayoutProps> = ({ children }) => {
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
+  return (
+    <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
+      <Script onLoad={() => setIsScriptLoaded(true)} src="https://telegram.org/js/telegram-web-app.js" />
+      {isScriptLoaded && (
+        <SDKProvider>
+          <TelegramInit />
+          <Suspense fallback={<BrandLoader />}>{children}</Suspense>
+          <LayoutModalContainer />
+        </SDKProvider>
+      )}
+    </main>
+  );
+};
 
 export default MainLayout;
