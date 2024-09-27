3fd49eaed3a17aed32ad7e839ec8f38736378498
Iurii Venger <venger.yuriy@icloud.com>
2024-09-28 00:41:32 +0200

    update script strategy

--- src/app/(telegram_layout)/layout.tsx
+++ src/app/(telegram_layout)/layout.tsx
@@ -13,29 +13,26 @@ import { themes } from '@/config/themes';
 
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
-const MainLayout: FC<RootLayoutProps> = ({ children }) => {
-  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+const MainLayout: FC<RootLayoutProps> = ({ children }) => (
+  // const [isScriptLoaded, setIsScriptLoaded] = useState(false);
 
-  useEffect(() => {
-    if (isScriptLoaded) {
-      (window as any).Telegram.WebApp.setBackgroundColor(themes.dark.baseColors.background);
-      (window as any).Telegram.WebApp.setHeaderColor(themes.dark.baseColors.background);
-      (window as any).Telegram.WebApp.setBottomBarColor(themes.dark.baseColors.background);
-    }
-  }, [isScriptLoaded]);
+  // useEffect(() => {
+  //   if (isScriptLoaded) {
+  //     (window as any).Telegram.WebApp.setBackgroundColor(themes.dark.baseColors.background);
+  //     (window as any).Telegram.WebApp.setHeaderColor(themes.dark.baseColors.background);
+  //     (window as any).Telegram.WebApp.setBottomBarColor(themes.dark.baseColors.background);
+  //   }
+  // }, [isScriptLoaded]);
 
-  return (
-    <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
-      <Script onLoad={() => setIsScriptLoaded(true)} src="https://telegram.org/js/telegram-web-app.js" />
-      {isScriptLoaded && (
-        <SDKProvider>
-          <TelegramInit />
-          <Suspense fallback={<BrandLoader />}>{children}</Suspense>
-          <LayoutModalContainer />
-        </SDKProvider>
-      )}
-    </main>
-  );
-};
-
+  <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
+    <Script strategy="beforeInteractive" src="https://telegram.org/js/telegram-web-app.js" />
+    {/* {isScriptLoaded && ( */}
+    <SDKProvider>
+      <TelegramInit />
+      <Suspense fallback={<BrandLoader />}>{children}</Suspense>
+      <LayoutModalContainer />
+    </SDKProvider>
+    {/* )} */}
+  </main>
+);
 export default MainLayout;
