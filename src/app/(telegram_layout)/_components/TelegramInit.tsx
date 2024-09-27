d5d8851b26fe56c7f49278884c2e375bbe118834
Iurii Venger <venger.yuriy@icloud.com>
2024-09-27 21:22:37 +0200

    integrate pathThemeParams

--- src/app/(telegram_layout)/_components/TelegramInit.tsx
+++ src/app/(telegram_layout)/_components/TelegramInit.tsx
@@ -1,4 +1,5 @@
 import {
+  parseThemeParams,
   useInitData,
   useLaunchParams,
   useMiniApp,
@@ -106,8 +107,23 @@ const TelegramInit = () => {
 
   useEffect(() => {
     if (miniApp) {
-      miniApp.setBgColor('#000000');
-      miniApp.setHeaderColor('#000000');
+      // miniApp.setBgColor('#000000');
+      // miniApp.setHeaderColor('#000000');
+      parseThemeParams({
+        accent_text_color: '#6ab2f2',
+        bg_color: '#17212b',
+        button_color: '#5288c1',
+        button_text_color: '#ffffff',
+        destructive_text_color: '#ec3942',
+        header_bg_color: '#17212b',
+        hint_color: '#708499',
+        link_color: '#6ab3f3',
+        secondary_bg_color: '#232e3c',
+        section_bg_color: '#17212b',
+        section_header_text_color: '#6ab3f3',
+        subtitle_text_color: '#708499',
+        text_color: '#f5f5f5',
+      });
       toast.success('miniApp.setHeaderColor and miniApp.setBgColor');
     }
   }, [miniApp]);
