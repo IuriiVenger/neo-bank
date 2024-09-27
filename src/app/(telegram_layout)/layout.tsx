2a259061678952446aa2769db8aa6af52d612bea
Iurii Venger <venger.yuriy@icloud.com>
2024-09-28 00:49:46 +0200

    add Telegram interface to window object

--- src/app/(telegram_layout)/layout.tsx
+++ src/app/(telegram_layout)/layout.tsx
@@ -3,7 +3,7 @@
 import 'react-toastify/dist/ReactToastify.css';
 import { SDKProvider } from '@telegram-apps/sdk-react';
 import Script from 'next/script';
-import { FC, Suspense, useEffect, useState } from 'react';
+import { FC, Suspense, useEffect } from 'react';
 
 import TelegramInit from './_components/TelegramInit';
 
@@ -14,26 +14,21 @@ import { themes } from '@/config/themes';
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
 const MainLayout: FC<RootLayoutProps> = ({ children }) => {
-  // const [isScriptLoaded, setIsScriptLoaded] = useState(false);
-
   useEffect(() => {
-    // if (isScriptLoaded) {
-    (window as any).Telegram.WebApp.setBackgroundColor(themes.dark.baseColors.background);
-    (window as any).Telegram.WebApp.setHeaderColor(themes.dark.baseColors.background);
-    (window as any).Telegram.WebApp.setBottomBarColor(themes.dark.baseColors.background);
-    // }
+    window.Telegram.WebApp.setBackgroundColor(themes.dark.baseColors.background);
+    window.Telegram.WebApp.setHeaderColor(themes.dark.baseColors.background);
+    window.Telegram.WebApp.setBottomBarColor(themes.dark.baseColors.background);
   }, []);
 
   return (
     <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
       <Script strategy="beforeInteractive" src="https://telegram.org/js/telegram-web-app.js" />
-      {/* {isScriptLoaded && ( */}
+
       <SDKProvider>
         <TelegramInit />
         <Suspense fallback={<BrandLoader />}>{children}</Suspense>
         <LayoutModalContainer />
       </SDKProvider>
-      {/* )} */}
     </main>
   );
 };
