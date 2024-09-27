4934b6df1310f601388dbdb92593709fd5cd9c1f
Iurii Venger <venger.yuriy@icloud.com>
2024-09-27 20:33:21 +0200

    update parseThemeParams

--- src/app/(telegram_layout)/_components/TelegramInit.tsx
+++ src/app/(telegram_layout)/_components/TelegramInit.tsx
@@ -64,22 +64,6 @@ import { setAppEnviroment } from '@/store/slices/config';
 //   platform: 'tdesktop',
 // });
 
-parseThemeParams({
-  accent_text_color: '#6ab2f2',
-  bg_color: '#17212b',
-  button_color: '#5288c1',
-  button_text_color: '#ffffff',
-  destructive_text_color: '#ec3942',
-  header_bg_color: '#17212b',
-  hint_color: '#708499',
-  link_color: '#6ab3f3',
-  secondary_bg_color: '#232e3c',
-  section_bg_color: '#17212b',
-  section_header_text_color: '#6ab3f3',
-  subtitle_text_color: '#708499',
-  text_color: '#f5f5f5',
-});
-
 const TelegramInit = () => {
   const { isWebAppInitialized } = useAppSelector(selectConfig);
   const isUserLoggedIn = useAppSelector(selectIsUserLoggedIn);
@@ -116,6 +100,21 @@ const TelegramInit = () => {
   useEffect(() => {
     dispatch(setAppEnviroment(AppEnviroment.TELEGRAM));
     localStorage.setItem('app_enviroment', AppEnviroment.TELEGRAM);
+    parseThemeParams({
+      accent_text_color: '#6ab2f2',
+      bg_color: '#17212b',
+      button_color: '#5288c1',
+      button_text_color: '#ffffff',
+      destructive_text_color: '#ec3942',
+      header_bg_color: '#17212b',
+      hint_color: '#708499',
+      link_color: '#6ab3f3',
+      secondary_bg_color: '#232e3c',
+      section_bg_color: '#17212b',
+      section_header_text_color: '#6ab3f3',
+      subtitle_text_color: '#708499',
+      text_color: '#f5f5f5',
+    });
   }, []);
 
   return null;
