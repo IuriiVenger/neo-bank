ee1a54f6da82ec0f8bb9cb7ed1ef4cb354f5564e
Iurii Venger <venger.yuriy@icloud.com>
2024-09-27 19:41:24 +0200

    update modalStructure, update onOpenChangeHandler in mainModal

---
+++ src/components/modals/MainModal/TelegramModal.tsx
@@ -0,0 +1,163 @@
+import { cn, Modal, ModalBody, ModalContent, ModalHeader } from '@nextui-org/react';
+import { useBackButton, useMainButton } from '@telegram-apps/sdk-react';
+import { FC, useEffect } from 'react';
+
+import { MainModalProps } from '.';
+
+import { themes } from '@/config/themes';
+
+const TelegramModal: FC<MainModalProps> = (props) => {
+  const {
+    children,
+    header,
+    isOpen,
+    onOpenChange,
+    contentClassName,
+    bodyClassname,
+    onConfirm,
+    confirmButtonText,
+    confirmButtonDisabled,
+    confirmButtonHidden,
+    isAppFullInitialized,
+    isLoading,
+    onClose,
+    ...otherProps
+  } = props;
+
+  if (!isAppFullInitialized) return null;
+
+  const backButton = useBackButton();
+  const mainButton = useMainButton();
+
+  const confirmHandler = () => {
+    onConfirm && onConfirm();
+  };
+
+  const closeModal = () => {
+    onOpenChange && onOpenChange(false);
+    onClose && onClose();
+  };
+
+  const onConfirmButtonTextChanged = () => {
+    if (!mainButton || !confirmButtonText || !isOpen) return;
+
+    mainButton.setText(confirmButtonText);
+  };
+
+  const disableMainButton = () => {
+    if (!mainButton || !isOpen) return;
+
+    mainButton.disable();
+
+    mainButton.setBgColor(themes.light.telegramColors.mainButton.disabledColor);
+  };
+
+  const enableMainButton = () => {
+    if (!mainButton || !isOpen) return;
+
+    mainButton.enable();
+    mainButton.setBgColor(themes.light.telegramColors.mainButton.color);
+  };
+
+  const showLoader = () => {
+    if (!mainButton || !isOpen) return;
+
+    mainButton.showLoader();
+    disableMainButton();
+    mainButton.setText('Loading...');
+  };
+
+  const hideLoader = () => {
+    if (!mainButton || !isOpen) return;
+
+    mainButton.hideLoader();
+    enableMainButton();
+    onConfirmButtonTextChanged();
+  };
+
+  const onConfirmButtonDisabledChanged = () => {
+    confirmButtonDisabled ? disableMainButton() : enableMainButton();
+  };
+
+  const onIsLoadingChanged = () => {
+    if (!mainButton || !isOpen) return;
+
+    isLoading ? showLoader() : hideLoader();
+  };
+
+  const onOnConfirmChanged = () => {
+    if (!mainButton || !isOpen) return;
+    mainButton.off('click', confirmHandler);
+    mainButton.on('click', confirmHandler);
+  };
+
+  const onOpenChangeHandler = () => {
+    if (!backButton || !mainButton) return;
+
+    if (isOpen) {
+      backButton.show();
+
+      backButton.on('click', closeModal);
+      if (!confirmButtonHidden) {
+        mainButton.show();
+        onOnConfirmChanged();
+        onConfirmButtonTextChanged();
+        onConfirmButtonDisabledChanged();
+      }
+    } else {
+      backButton.hide();
+      backButton.off('click', closeModal);
+      mainButton.enable();
+      mainButton.hideLoader();
+      if (!confirmButtonHidden) {
+        mainButton.off('click', confirmHandler);
+        mainButton.hide();
+      }
+    }
+  };
+
+  useEffect(() => {
+    onOpenChangeHandler();
+
+    return () => {
+      if (backButton) {
+        backButton.off('click', closeModal);
+      }
+      if (mainButton) {
+        mainButton.off('click', confirmHandler);
+      }
+    };
+  }, [isOpen, confirmButtonHidden]);
+
+  useEffect(() => () => onOpenChangeHandler(), []);
+
+  useEffect(() => {
+    onConfirmButtonTextChanged();
+  }, [confirmButtonText]);
+
+  useEffect(() => {
+    onConfirmButtonDisabledChanged();
+  }, [confirmButtonDisabled]);
+
+  useEffect(() => {
+    onOnConfirmChanged();
+    return () => {
+      mainButton.off('click', confirmHandler);
+    };
+  }, [onConfirm]);
+
+  useEffect(() => {
+    onIsLoadingChanged();
+  }, [isLoading]);
+
+  return (
+    <Modal isOpen={isOpen} hideCloseButton disableAnimation onOpenChange={onOpenChange} {...otherProps}>
+      <ModalContent className={cn('fixed left-0 top-0 max-h-svh md:relative md:max-h-[90vh]', contentClassName)}>
+        {!!header && <ModalHeader>{header}</ModalHeader>}
+        <ModalBody className={cn('pb-10 shadow-inner sm:max-h-[90vh]', bodyClassname)}>{children}</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default TelegramModal;
---
+++ src/components/modals/MainModal/index.tsx
@@ -0,0 +1,76 @@
+import { ModalProps } from '@nextui-org/react';
+import cn from 'classnames';
+import { FC, memo, ReactNode, useEffect } from 'react';
+
+import TelegramModal from './TelegramModal';
+
+import WebModal from './WebModal';
+
+import { framerMotionAnimations } from '@/config/animations';
+
+import { AppEnviroment } from '@/constants';
+import useBreakpoints from '@/hooks/useBreakpoints';
+import { useAppSelector } from '@/store';
+import { selectConfig } from '@/store/selectors';
+
+export type HiddenConfirmButtonProps = {
+  confirmButtonHidden: true;
+  onConfirm?: never;
+  confirmButtonText?: never;
+};
+
+export type VisibleConfirmButtonProps = {
+  confirmButtonHidden?: boolean;
+  onConfirm: () => void;
+  confirmButtonText: string;
+};
+
+export type CustomModalProps = ModalProps & {
+  header?: ReactNode | string;
+  footer?: ReactNode | string;
+  contentClassName?: string;
+  bodyClassname?: string;
+};
+
+export type MainModalProps = CustomModalProps &
+  (HiddenConfirmButtonProps | VisibleConfirmButtonProps) & {
+    confirmButtonDisabled?: boolean;
+    isLoading?: boolean;
+    isAppFullInitialized?: boolean;
+    nativeCloseButton?: boolean;
+    saveScrollPosition?: boolean;
+    isDismissable?: boolean;
+  };
+
+const MainModal: FC<MainModalProps> = (props) => {
+  const { mdBreakpoint } = useBreakpoints();
+  const { appEnviroment, isAppFullInitialized } = useAppSelector(selectConfig);
+  const { size, motionProps, isOpen, className, nativeCloseButton = true, saveScrollPosition } = props;
+
+  const responsiveSize = mdBreakpoint ? 'md' : 'full';
+  const modalSize = size || responsiveSize;
+  const responsiveMotionProps = mdBreakpoint ? { variants: framerMotionAnimations.downEnterExit } : undefined;
+  const modalMotionProps = motionProps || responsiveMotionProps;
+  const isTelegramEnviroment = appEnviroment === AppEnviroment.TELEGRAM;
+  const disableAnimation = !mdBreakpoint && !motionProps;
+
+  useEffect(() => {
+    if (isOpen && !mdBreakpoint && window && !saveScrollPosition) {
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    }
+  }, [isOpen]);
+
+  const modifiedProps = {
+    ...props,
+    nativeCloseButton,
+    isAppFullInitialized,
+    disableAnimation,
+    motionProps: modalMotionProps,
+    size: modalSize,
+    className: cn('bg-background', className),
+  };
+
+  return isTelegramEnviroment ? <TelegramModal {...modifiedProps} /> : <WebModal {...modifiedProps} />;
+};
+
+export default memo(MainModal);
