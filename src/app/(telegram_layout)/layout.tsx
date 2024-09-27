c23f76f4ce78148cc156b210fa2a2e3d987d03ac
Iurii Venger <venger.yuriy@icloud.com>
2024-09-28 00:06:07 +0200

    disable script

--- src/app/(telegram_layout)/layout.tsx
+++ src/app/(telegram_layout)/layout.tsx
@@ -14,7 +14,7 @@ type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
 const MainLayout: FC<RootLayoutProps> = ({ children }) => (
   <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
-    <Script src="https://telegram.org/js/telegram-web-app.js" />
+    {/* <Script src="https://telegram.org/js/telegram-web-app.js" /> */}
     <SDKProvider>
       <TelegramInit />
       <Suspense fallback={<BrandLoader />}>{children}</Suspense>
