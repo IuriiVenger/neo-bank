cab66aa0898d70ff95fcbdc6497ea014b6a5eb39
Iurii Venger <venger.yuriy@icloud.com>
2024-09-28 00:26:58 +0200

    update telegram theme params

--- src/app/(telegram_layout)/layout.tsx
+++ src/app/(telegram_layout)/layout.tsx
@@ -3,17 +3,26 @@
 import 'react-toastify/dist/ReactToastify.css';
 import { SDKProvider } from '@telegram-apps/sdk-react';
 import Script from 'next/script';
-import { FC, Suspense, useState } from 'react';
+import { FC, Suspense, useEffect, useState } from 'react';
 
 import TelegramInit from './_components/TelegramInit';
 
 import LayoutModalContainer from '@/components/modals/LayoutModalContainer';
 import { BrandLoader } from '@/components/ui/Loader';
+import { themes } from '@/config/themes';
 
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
 const MainLayout: FC<RootLayoutProps> = ({ children }) => {
   const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
+  useEffect(() => {
+    if (isScriptLoaded) {
+      (window as any).Telegram.WebApp.setBackgroundColor(themes.dark.baseColors.background);
+      (window as any).Telegram.WebApp.setHeaderColor(themes.dark.baseColors.background);
+      (window as any).Telegram.WebApp.setBottomBarColor(themes.dark.telegramColors.mainButton.color);
+    }
+  }, [isScriptLoaded]);
 
   return (
     <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
