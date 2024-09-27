46a6b927af0262984d5c2e0b45997d0f4addb11e
Iurii Venger <venger.yuriy@icloud.com>
2024-09-27 20:25:19 +0200

    update parseLauncParams

--- src/app/(telegram_layout)/_components/TelegramInit.tsx
+++ src/app/(telegram_layout)/_components/TelegramInit.tsx
@@ -93,18 +93,45 @@ const TelegramInit = () => {
 
   useEffect(() => {
     if (isWebAppInitialized) {
-      parseLaunchParams([
-        'tgWebAppThemeParams',
-        JSON.stringify({
-          bg_color: '#17212b',
-          button_color: '#5288c1',
-          button_text_color: '#ffffff',
-          hint_color: '#708499',
-          link_color: '#6ab3f3',
-          secondary_bg_color: '#232e3c',
-          text_color: '#f5f5f5',
-        }),
-      ]);
+      parseLaunchParams(
+        new URLSearchParams([
+          ['tgWebAppVersion', '6.7'],
+          ['tgWebAppPlatform', 'tdekstop'],
+          ['tgWebAppBotInline', '1'],
+          [
+            'tgWebAppData',
+            new URLSearchParams([
+              ['query_id', 'AAHdF6IQAAAAAN0XohAOqR8k'],
+              [
+                'user',
+                JSON.stringify({
+                  id: 279058397,
+                  first_name: 'Vladislav',
+                  last_name: 'Kibenko',
+                  username: 'vdkfrost',
+                  language_code: 'ru',
+                  is_premium: true,
+                  allows_write_to_pm: true,
+                }),
+              ],
+              ['auth_date', '1691441944'],
+              ['hash', 'abc'],
+            ]).toString(),
+          ],
+          [
+            'tgWebAppThemeParams',
+            JSON.stringify({
+              bg_color: '#17212b',
+              button_color: '#5288c1',
+              button_text_color: '#ffffff',
+              hint_color: '#708499',
+              link_color: '#6ab3f3',
+              secondary_bg_color: '#232e3c',
+              text_color: '#f5f5f5',
+            }),
+          ],
+        ]),
+      );
     }
   }, [isWebAppInitialized]);
 
