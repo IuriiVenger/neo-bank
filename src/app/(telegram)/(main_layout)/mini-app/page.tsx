0259fc39d1a4964b2bab6aa98007883b3666a7d2
Iurii Venger <venger.yuriy@icloud.com>
2024-10-16 14:39:45 +0200

    add checkOnUnsupproted version

--- src/app/(telegram)/(main_layout)/mini-app/page.tsx
+++ src/app/(telegram)/(main_layout)/mini-app/page.tsx
@@ -4,11 +4,14 @@ import { useLaunchParams } from '@telegram-apps/sdk-react';
 
 import DashboardPage from '@/app/(common_pages)/_dashboard';
 import privateRoute from '@/components/privateRoute';
+import { unsupportedTelegramVersions } from '@/config/telegram';
 
 const MiniAppPage = () => {
   const launchParams = useLaunchParams(true);
 
-  console.log('launchParams', launchParams);
+  if (launchParams?.version && unsupportedTelegramVersions.includes(launchParams.version)) {
+    return <h1>Plese update your app</h1>;
+  }
 
   return <DashboardPage />;
 };
