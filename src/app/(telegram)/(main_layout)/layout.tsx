9b64595072b992118a5ff0464cbbba9324080a74
Iurii Venger <venger.yuriy@icloud.com>
2024-10-16 16:33:39 +0200

    update ChechkTelegramVersion place

--- src/app/(telegram)/(main_layout)/layout.tsx
+++ src/app/(telegram)/(main_layout)/layout.tsx
@@ -4,13 +4,16 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import { FC, Suspense } from 'react';
 
+import WithCheckTGVersion from '@/components/initialization/WithCheckTGVersion';
 import { BrandLoader } from '@/components/ui/Loader';
 
 type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
 
 const MainLayout: FC<RootLayoutProps> = ({ children }) => (
   <main className="flex w-full max-w-screen-2xl flex-grow justify-center p-5 pb-20 md:px-10 md:pt-8">
-    <Suspense fallback={<BrandLoader />}>{children}</Suspense>
+    <Suspense fallback={<BrandLoader />}>
+      <WithCheckTGVersion>{children}</WithCheckTGVersion>
+    </Suspense>
   </main>
 );
 
