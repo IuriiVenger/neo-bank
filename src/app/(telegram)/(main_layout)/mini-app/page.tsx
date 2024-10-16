07ed604598a4945c3ae9b567b0d3fe6d7825ed46
Iurii Venger <venger.yuriy@icloud.com>
2024-10-16 14:30:28 +0200

    add mini-app check

--- src/app/(telegram)/(main_layout)/mini-app/page.tsx
+++ src/app/(telegram)/(main_layout)/mini-app/page.tsx
@@ -1,8 +1,16 @@
 'use client';
 
+import { useLaunchParams } from '@telegram-apps/sdk-react';
+
 import DashboardPage from '@/app/(common_pages)/_dashboard';
 import privateRoute from '@/components/privateRoute';
 
-const MiniAppPage = () => <DashboardPage />;
+const MiniAppPage = () => {
+  const launchParams = useLaunchParams(true);
+
+  console.log('launchParams', launchParams);
+
+  return <DashboardPage />;
+};
 
 export default privateRoute(MiniAppPage);
