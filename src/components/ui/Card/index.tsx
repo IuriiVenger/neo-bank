b43ef26ced8f862177f7d9218d10fc9b7b914f06
Iurii Venger <venger.yuriy@icloud.com>
2024-10-24 13:58:45 +0200

    fix card sizes

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -2,10 +2,10 @@ import cn from 'classnames';
 import Image from 'next/image';
 import { FC } from 'react';
 
-import { CardSizes, CardSizesValues, CardProvider } from './types';
+import { CardProviders, cardSizesMap } from './config';
 
-import masterCardLogo from '@/assets/svg/payment-systems/master-card-logo.svg';
-import visaLogo from '@/assets/svg/payment-systems/visa-logo-white.svg';
+import { CardProps } from './types';
+
 import darkCardBackground from '@/assets/tenant/dark/card-background.webp';
 import darkCardLogo from '@/assets/tenant/dark/card-logo.svg';
 import lightCardBackground from '@/assets/tenant/light/card-background.webp';
@@ -13,69 +13,6 @@ import lightCardLogo from '@/assets/tenant/light/card-logo.svg';
 import ThemeImage from '@/components/ui/ThemeImage';
 import { CardStatus } from '@/constants';
 import { getCardProvider } from '@/utils/financial';
-
-const cardSizesMap: Record<CardSizes, Record<CardSizesValues, string>> = {
-  xs: {
-    provider: 'h-1 w-fit',
-    tenantLogo: 'h-1 w-fit',
-    card: 'w-15 h-9 rounded-[2px] p-1.5',
-    cardNumber: 'text-[4px]',
-    balance: 'hidden',
-    balanceLabel: 'hidden',
-  },
-  sm: {
-    provider: 'h-3 w-fit',
-    tenantLogo: 'h-2.5 w-fit',
-    card: 'w-44 h-28 rounded-[7px] p-2.5',
-    cardNumber: 'text-xs',
-    balance: 'text-sm',
-    balanceLabel: 'text-[10px]',
-  },
-  md: {
-    provider: 'h-5.5 w-fit',
-    tenantLogo: 'h-5 w-fit',
-    card: 'w-78 h-49 rounded-[12px] p-5',
-    cardNumber: 'text-base',
-    balance: 'text-sm mt-1',
-    balanceLabel: 'text-[8px]',
-  },
-  lg: {
-    provider: 'h-7 w-fit',
-    tenantLogo: 'h-5 w-fit',
-    card: 'w-90 h-57 rounded-[14px] p-5',
-    cardNumber: 'text-xl',
-    balance: 'text-2xl mt-1.5',
-    balanceLabel: 'text-sm',
-  },
-  adaptive: {
-    provider: 'h-3 w-fit lg:h-5.5 lg:w-fit',
-    tenantLogo: 'h-2.5 w-fit lg:h-5 lg:w-fit',
-    card: 'w-44 h-28 lg:w-78 lg:h-49 rounded-[7px] p-2.5 lg:p-5 lg:rounded-[12px]',
-    cardNumber: 'text-xs lg:text-base',
-    balance: 'text-sm lg:text-lg lg:mt-1.5',
-    balanceLabel: 'text-[8px] lg:text-sm',
-  },
-};
-
-const CardProviders: Record<CardProvider, string> = {
-  Visa: visaLogo,
-  Mastercard: masterCardLogo,
-};
-
-type CardProps = {
-  className?: string;
-  size?: CardSizes;
-  disabled?: boolean;
-  blocked?: boolean;
-  provider?: CardProvider;
-  cardNumber?: string;
-  balance?: string | null;
-  // CVV?: string;
-  // expirationDate?: string;
-  masked?: boolean;
-  // tenantLogo?: string;
-  status?: CardStatus;
-};
 
 const Card: FC<CardProps> = (props) => {
   const {
--- src/components/ui/Card/types.ts
+++ src/components/ui/Card/types.ts
@@ -1,4 +1,19 @@
+import { CardStatus } from '@/constants';
+
 export type CardSizes = 'xs' | 'sm' | 'md' | 'lg' | 'adaptive';
 export type CardSizesValues = 'provider' | 'tenantLogo' | 'card' | 'cardNumber' | 'balance' | 'balanceLabel';
 export type DetailCardSizesValues = 'provider' | 'tenantLogo' | 'card' | 'cardNumber';
 export type CardProvider = 'Visa' | 'MasterCard' | string;
+
+export type CardProps = {
+  className?: string;
+  size?: CardSizes;
+  disabled?: boolean;
+  blocked?: boolean;
+  provider?: CardProvider;
+  cardNumber?: string;
+  balance?: string | null;
+  masked?: boolean;
+  status?: CardStatus;
+  expirationDate?: string;
+};
